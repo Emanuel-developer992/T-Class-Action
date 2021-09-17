00fe319fcb900838cc7698b92ff728bd9e433a6a
Emanuel-developer992 <Orion_Developer@outlook.com>
2021-09-17 09:07:53 -0300

    first commit

---
+++ js/tcssAction.js
@@ -0,0 +1,14 @@
+//EXECUTAR AO CARREGAR A PÁGINA
+window.onload = function() {
+
+    console.log("--------");
+    console.log(getFormMode());
+    console.log(getMobile());
+    console.log(getWKNumState());
+    console.log(getWKUser());
+    console.log(getWKNumProces());
+    console.log(getWKUserLocale());
+    console.log(getWKCardId());
+    console.log("--------");
+    
+    };
